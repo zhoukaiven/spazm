421cb157819cbf542bd88c6e662634cf3976ddd4
zhoukaiven <zhou.kaiven@gmail.com>
2013-06-11 23:30:25 -0400

    replace safe_print with to_str; fixed alternate method to get stream qualities

--- spazm.py
+++ spazm.py
@@ -15,17 +15,11 @@ class Spazm():
 			self.ACCESS_TOKEN = token
 		self.twitch = twitchingpython.TwitchingWrapper(self.ACCESS_TOKEN)
 
-	def safe_print(self, phrase):
-		try:
-			print str(phrase)
-		except UnicodeEncodeError: # obj is unicode
-			print unicode(phrase).encode('unicode_escape')
-	
 	def to_str(self, obj):
 		try:
-			return str(phrase)
+			return str(obj)
 		except UnicodeEncodeError: # obj is unicode
-			return unicode(phrase).encode('unicode_escape')
+			return unicode(obj).encode('unicode_escape')
 			
 	def run(self, cmd):
 		startupinfo = None
@@ -44,10 +38,10 @@ class Spazm():
 				channel = streams_followed[i]['channel']
 				
 				stream = {}
-				stream['streamer'] = channel['display_name']
-				stream['status'] = channel['status']
-				stream['game'] = channel['game']
-				stream['url'] = channel['url']
+				stream['streamer'] = self.to_str(channel['display_name'])
+				stream['status'] = self.to_str(channel['status'])
+				stream['game'] = self.to_str(channel['game'])
+				stream['url'] = self.to_str(channel['url'])
 
 				streams.append(stream)
 			
@@ -58,22 +52,22 @@ class Spazm():
 			print "=== Streams Followed ==="
 			for i in range(0, len(streams)):
 				stream = streams[i]
-				self.safe_print("%s) %s [%s]" % (i, stream['streamer'], stream['game']))
-				self.safe_print(stream['status'])
+				print "%s) %s [%s]" % (i, stream['streamer'], stream['game'])
+				print stream['status']
 				print
 				
-			print "\nChoose: ",
+			print "Choose: ",
 			input = raw_input()
 			print
 			
 			#Display qualities for the stream chosen
-			url = streams[int(input)]['url']
+			url = self.to_str(streams[int(input)]['url'])
 			
 			plugin = livestreamer.resolve_url(url)
 			qualities = plugin.get_streams().keys()
 			if not qualities: #IDK why, sometimes livestreamer API doesnt work, so I manually run livestreamer
-				qualities, stderr = Popen("livestreamer %s" % url, stdout=PIPE, stderr=PIPE) #possibly opens new cmd prompt
-			self.safe_print("Qualities: %s" %qualities)
+				qualities, stderr = Popen("livestreamer %s" % url, stdout=PIPE, stderr=PIPE).communicate()
+			print self.to_str("Qualities: %s" %qualities)
 			
 			print "\nChoose: ",
 			input = raw_input()
