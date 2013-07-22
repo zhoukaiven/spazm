d174c315a0c4ce4b70a29a507582ed29688bfacd
zhoukaiven <zhou.kaiven@gmail.com>
2013-07-21 23:47:17 -0400

    qualities fetched in Threads

--- stream.py
+++ stream.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
+
+from threading import Thread, Condition
 import os
 import subprocess
 from subprocess import check_call, Popen, PIPE
@@ -12,6 +14,11 @@ class Stream(object):
 		
 		self.qualities = None
 		
+		self.thread_condition = Condition()
+		self.thread = Thread(target = self.get_stream_qualities)
+		self.thread.daemon = True
+		self.thread.start()
+		
 	def update(self, channel_data):
 		channel = channel_data['channel']
 		
@@ -22,7 +29,7 @@ class Stream(object):
 	
 	def run(self, cmd):
 		startupinfo = None
-		if os.name == 'nt':
+		if os.name == 'nt': #might be redundant due to threading
 			startupinfo = subprocess.STARTUPINFO()
 			startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 		process = subprocess.Popen(cmd, startupinfo=startupinfo, stdout=PIPE, stderr=PIPE)
@@ -33,6 +40,8 @@ class Stream(object):
 		return buffer
 
 	def get_stream_qualities(self):
+		self.thread_condition.acquire()
+		
 		'''	 #can't use this until livestreamer python api supports flash streams
 		livestreamer = Livestreamer()
 		plugin = livestreamer.resolve_url(self.url)
@@ -48,8 +57,23 @@ class Stream(object):
 			self.qualities = [x.strip() for x in self.qualities.split(',')]
 		elif no_streams:
 			self.qualities = -1
+		
+		self.thread_condition.notify()
+		self.thread_condition.release()
 			
 	def load_qualities_buffer(self):
+		self.thread_condition.acquire()
+		while not self.qualities:
+			self.thread_condition.wait()
+		
+		if self.qualities == -1:
+			return None
+		else:
+			buffer = list(self.qualities) #create a copy
+			for i, quality in enumerate(buffer):
+				buffer[i] = "%s) %s"%( (i+1), quality )
+			return buffer
+		'''
 		if self.qualities:
 			buffer = list(self.qualities) #create a copy
 			for i, quality in enumerate(buffer):
@@ -60,6 +84,7 @@ class Stream(object):
 		else:
 			self.get_stream_qualities()
 			return self.load_qualities_buffer()
+		'''
 		
 	def watch(self, quality):
 		self.run("livestreamer %s %s" % (self.url, self.qualities[quality])) #does not wait to complete
